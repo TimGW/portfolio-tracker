12a514a41a681320c0e78c4c3bde227d7bb91522
Tim Gortworst <tim.gortworst@gmail.com>
2021-03-03 17:29:24 +0100

    added info blocks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @push('css')
-	<link href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
+<link href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
 @endpush
 
 @section('content')
@@ -9,7 +9,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-6 mt-3">
-            <div class="card">
+            <div class="card h-100">
                 <div class="card-header">{{ __('Sectorverdeling') }}</div>
 
                 <div class="card-body">
@@ -17,12 +17,46 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-6 mt-3">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="col-lg-6">
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col mt-3">
+                     <div class="card h-100 text-center">
+                <div class="card-header">{{ __('Balans') }}</div>
+                     
+                        <div class="card-body">
+                            <h3 class="card-title">€ 10000</h3>
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="col mt-3">
+                     <div class="card h-100 text-center">
+                <div class="card-header">{{ __('Groei') }}</div>
 
-                <div class="card-body">
+                        <div class="card-body">
+                            <h3 class="card-title">- 2,5%</h3>
+                           
+                        </div>
+                    </div>
+                </div>
+                <div class="col mt-3">
+                     <div class="card h-100 text-center">
+                <div class="card-header">{{ __('Gemiddeld dividend') }}</div>
 
+                        <div class="card-body">
+                            <h3 class="card-title">+ 3,07%</h3> 
+                        </div>
+                    </div>
+                </div>
+                <div class="col mt-3">
+                     <div class="card h-100 text-center">
+                <div class="card-header">{{ __('Jaarlijks inkomen') }}</div>
+
+                        <div class="card-body">
+                            <h3 class="card-title">€ 500</h3>
+                        
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -47,14 +81,14 @@
                         </thead>
                         <tbody>
                             @foreach ($stocks as $stock)
-                                <tr>
-                                    <td>{{$stock->product}}</td>
-                                    <td>{{$stock->symbol_isin}}</td>
-                                    <td>{{$stock->quantity}}</td>
-                                    <td>{{$stock->closing_price}}</td>
-                                    <td>{{$stock->local_value}}</td>
-                                    <td>{{$stock->value_in_euros}}</td>
-                                </tr>
+                            <tr>
+                                <td>{{$stock->product}}</td>
+                                <td>{{$stock->symbol_isin}}</td>
+                                <td>{{$stock->quantity}}</td>
+                                <td>{{$stock->closing_price}}</td>
+                                <td>{{$stock->local_value}}</td>
+                                <td>{{$stock->value_in_euros}}</td>
+                            </tr>
                             @endforeach
                         </tbody>
                     </table>
