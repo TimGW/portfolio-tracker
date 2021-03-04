ac3cc68aa62ffef45d83b974b831283a2fbf556a
Tim Gortworst <tim.gortworst@gmail.com>
2021-03-04 21:03:47 +0100

    dutch lang

--- public/js/portfolio-datatable.js
+++ public/js/portfolio-datatable.js
@@ -1,5 +1,8 @@
 $(function () {
     $('#data-table-responsive').DataTable({
-        responsive: true
+        responsive: true,
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.10.22/i18n/Dutch.json'
+        }
     });
 });
