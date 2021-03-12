5846232a2347066e041adac7354230d9930937dc
Tim Gortworst <tim.gortworst@gmail.com>
2021-03-12 15:23:25 +0100

    skip invalid import

--- app/Imports/DeGiroDataImport.php
+++ app/Imports/DeGiroDataImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Transaction;
+use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 use Illuminate\Support\Facades\Auth;
@@ -16,23 +17,25 @@ class DeGiroDataImport implements ToModel, WithStartRow
      */
     public function model(array $row)
     {
-        return new Transaction([
-            'purchased_date' => $row[0],
-            'purchased_time' => $row[1],
-            'product' => $row[2],
-            'isin' => $row[3],
-            'exchange' => $row[4],
-            'place_of_execution' => $row[5],
-            'quantity' => $row[6],
-            'closing_rate' => str_replace(',', '.', $row[7]),
-            'local_value' => str_replace(',', '.', $row[9]),
-            'value' => str_replace(',', '.', $row[11]),
-//            'exchange_rate' => isset($row[13]) ? '' : str_replace(',', '.', $row[13]),
-//            'service_fee' => $row[14] ? null : str_replace(',', '.', $row[14]),
-            'total' => str_replace(',', '.', $row[16]),
-            'currency' => $row[8],
-            'user_id' => Auth::id()
-        ]);
+        try {
+            return new Transaction([
+                'purchased_date' => $row[0],
+                'purchased_time' => $row[1],
+                'product' => $row[2],
+                'isin' => $row[3],
+                'exchange' => $row[4],
+                'place_of_execution' => $row[5],
+                'quantity' => $row[6],
+                'closing_rate' => str_replace(',', '.', $row[7]),
+                'local_value' => str_replace(',', '.', $row[9]),
+                'value' => str_replace(',', '.', $row[11]),
+                'total' => str_replace(',', '.', $row[16]),
+                'currency' => $row[8],
+                'user_id' => Auth::id()
+            ]);
+        } catch (\Exception $e) {
+            return null;
+        }
     }
 
     /**
